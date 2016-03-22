95553e608437c85395e67d411a6582fe78129a2b
Dezypher <ccs@CCSs-iMac.local>
2016-03-22 16:03:52 +0800

    Working on Quest UI

--- Assets/Scripts/Quest/QuestAdapter.js
+++ Assets/Scripts/Quest/QuestAdapter.js
@@ -22,21 +22,21 @@ class QuestAdapter extends MonoBehaviour {
 		//update the View with the data from the Model
 		var demandCargo : Cargo = (Resources.Load("Reference/CargoReference") as GameObject).GetComponent(CargoRefScript).cargos[questModel.requiredCargoID];
 		var rewardCargo : Cargo = (Resources.Load("Reference/CargoReference") as GameObject).GetComponent(CargoRefScript).cargos[questModel.rewardCargoID];
-		questView.transform.GetChild(1).transform.GetChild(0).GetComponent(UI.Image).sprite = demandCargo.sprite; //Demand Cargo Image
+		questView.transform.GetChild(2).transform.GetChild(0).GetComponent(UI.Image).sprite = demandCargo.sprite; //Demand Cargo Image
 		if(questModel.requiredCargoAmount > 0)
-			questView.transform.GetChild(1).transform.GetChild(1).GetComponent(UI.Text).text = "x " + questModel.requiredCargoAmount; //update the Quantity Text to the amount
+			questView.transform.GetChild(2).transform.GetChild(1).GetComponent(UI.Text).text = "x " + questModel.requiredCargoAmount; //update the Quantity Text to the amount
 		else
-			questView.transform.GetChild(1).transform.GetChild(1).GetComponent(UI.Text).text = "Complete!";
-		questView.transform.GetChild(2).transform.GetChild(0).GetComponent(UI.Image).sprite = rewardCargo.sprite; //Reward Cargo Image
-		questView.transform.GetChild(2).transform.GetChild(1).GetComponent(UI.Text).text = "x " + questModel.rewardCargoAmount;
+			questView.transform.GetChild(2).transform.GetChild(1).GetComponent(UI.Text).text = "Complete!";
+		questView.transform.GetChild(3).transform.GetChild(0).GetComponent(UI.Image).sprite = rewardCargo.sprite; //Reward Cargo Image
+		questView.transform.GetChild(3).transform.GetChild(1).GetComponent(UI.Text).text = "x " + questModel.rewardCargoAmount;
 			
-		questView.transform.GetChild(0).GetComponent(UI.Text).text = questModel.location;
+		questView.transform.GetChild(1).GetComponent(UI.Text).text = questModel.location;
 		if(type == 2) {
-			questView.transform.GetChild(3).transform.GetChild(0).GetComponent(UI.Text).fontSize = 50;
-			if(!questModel.accomplished)
-				questView.transform.GetChild(3).transform.GetChild(0).GetComponent(UI.Text).text = "Abandon";
-			else
-				questView.transform.GetChild(3).transform.GetChild(0).GetComponent(UI.Text).text = "Claim";
+			//questView.transform.GetChild(4).transform.GetChild(0).GetComponent(UI.Text).fontSize = 50;
+			//if(!questModel.accomplished)
+				//questView.transform.GetChild(4).transform.GetChild(0).GetComponent(UI.Text).text = "Abandon";
+			//else
+				//questView.transform.GetChild(4).transform.GetChild(0).GetComponent(UI.Text).text = "Claim";
 		}
 	}
 
