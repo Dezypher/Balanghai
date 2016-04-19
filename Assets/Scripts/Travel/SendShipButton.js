d0d4757776972b4898b3832b5a8893c773c51874
Dezypher <Solomon@Solomons-MacBook-Pro.local>
2016-04-19 11:11:03 +0800

    Fixed Travel Send Screen

--- Assets/Scripts/Travel/SendShipButton.js
+++ Assets/Scripts/Travel/SendShipButton.js
@@ -2,13 +2,16 @@
 
 private var travelManager :TravelManager;
 private var shipListButton : ShipListButton;
+private var shipSendScreen : GameObject;
 
 function Awake(){
 	travelManager = GameObject.Find("ShipTravelManager")
 				.GetComponent(TravelManager);
 	shipListButton = GetComponent(ShipListButton);
+	shipSendScreen = GameObject.Find("SendShipUI");
 }
 
 function OnClick(){
 	travelManager.Send(shipListButton.shipIndex);
+	shipSendScreen.SetActive(false);
 }
