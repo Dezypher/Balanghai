1cc2c90635af4ef996e620a507ae4290d4273600
miloshakes <miguel_dungan@dlsu.edu.ph>
2016-07-11 14:03:30 +0800

    finally able to read the shits
    someone pls test this on other devices.

--- Assets/Scripts/Database/DataHandler.js
+++ Assets/Scripts/Database/DataHandler.js
@@ -3,26 +3,27 @@
  import System.Data;
  import Mono.Data.Sqlite;
  import System.Collections.Generic;
- 
+
  
  function Start () {
      var conn : String = "URI=file:" + Application.dataPath + "/DataLite.s3db"; //Path to database.
+     
      var dbconn : IDbConnection;
      dbconn = new SqliteConnection(conn);
      dbconn.Open(); //Open connection to the database.
      var dbcmd : IDbCommand = dbconn.CreateCommand();
-      
  
      var sqlQuery : String = "SELECT* From playerdata";
      dbcmd.CommandText = sqlQuery;
      var reader : IDataReader = dbcmd.ExecuteReader();
-     Debug.Log(reader.Read());
+    
       
      while (reader.Read())
      {
          var name : String = reader.GetString(0);
-         var score : int = reader.GetInt32(1);
-         Debug.Log( name+"+" +score );
+         var gold : float =reader.GetFloat(1);
+                  Debug.Log("PUTA"+name+"GOLD:"+gold);
+        // Debug.Log( name+"+" +score );
      }
       
      reader.Close();
